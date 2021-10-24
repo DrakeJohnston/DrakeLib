b602cb55d38c76b888c2e484bd94abef8949b211
drake <drakejwork@gmail.com>
2021-10-23 22:40:51 -0400

    Finished up GUI system

--- DrakeUtils/src/UtilTester.java
+++ DrakeUtils/src/UtilTester.java
@@ -9,35 +9,86 @@ public class UtilTester {
             {'0','0','0','E','x','i','t','0','0','0'},
             {'0','0','0','0','0','0','0','0','0','0'}
     };
+    static char[][] gamemenu = {
+            {'0','0','0','0','0','0','0','0','0','0'},
+            {'0','0','0','0','0','0','0','0','0','0'},
+            {'0','0','0','0','0','0','0','0','0','0'},
+            {'0','0','0','0','0','0','0','0','0','0'},
+            {'0','0','0','0','0','0','0','0','0','0'}
+    };
 
-    static  int[][] selPoints = {
+    static  int[][] mainMenuPoints = {
             {1,7},
             {3,7}
     };
 
-    public static GUI gui = new GUI(menu, selPoints);
+    public static GUI mainMenu = new GUI(menu, mainMenuPoints, true, '0');
+    public static GUI game = new GUI(gamemenu, false, '0');
+
+    public static  GUI[] guis = {
+            mainMenu,
+            game
+    };
 
     public static void main(String[] args){
 
         while(isRunning) {
-            gui.UpdateGUI();
-            gui.DisplayGUI();
 
-            Class<?>[] argTypes = {String.class};
-            Object[] arguments = {" "};
-            gui.getInput("UtilTester", "test", argTypes, arguments);
+            for (int i=0; i < guis.length; i++){
+                if(guis[i].IsActive() == true){
+
+                    Class<?>[] argTypes = {String.class};
+                    Object[] arguments = {" "};
+                    GUI gui = guis[i];
+
+                    gui.RunGUI();
+                    gui.getInput("UtilTester", "useInput", argTypes, arguments);
+                }
+            }
+
         }
     }
 
-    public static void test(String letter){
-        if(letter.equals("W")){
-            gui.setSelector(gui.getSelector() - 1);
-        }else if(letter.equals("S")){
-            gui.setSelector(gui.getSelector() + 1);
-        }else if(letter.equals("")){
-            if(gui.getSelector() == 1){
+    public static void useInput(String letter){
+        if(mainMenu.IsActive()) {
+            HandleMainMenuInput(letter);
+        }else if(game.IsActive()){
+            HandleGameInput(letter);
+        }
+    }
+
+    private static void HandleGameInput(String letter) {
+        int[] pos = game.getSelectorPos();
+        switch (letter){
+            case "W":
+                game.moveSelector(pos[0] - 1, pos[1]);
+                break;
+            case "A":
+                game.moveSelector(pos[0], pos[1] - 1);
+                break;
+            case "S":
+                game.moveSelector(pos[0] + 1, pos[1]);
+                break;
+            case "D":
+                game.moveSelector(pos[0], pos[1] + 1);
+                break;
+        }
+    }
+
+    private static void HandleMainMenuInput(String letter) {
+        if (letter.equals("W")) {
+            mainMenu.setSelector(mainMenu.getSelector() - 1);
+        } else if (letter.equals("S")) {
+            mainMenu.setSelector(mainMenu.getSelector() + 1);
+        } else if (letter.equals("")) {
+
+            if (mainMenu.getSelector() == 0) {
+                game.setActive(true);
+                mainMenu.setActive(false);
+            } else if (mainMenu.getSelector() == 1) {
                 isRunning = false;
             }
+
         }
     }
 }
--- DrakeUtils/src/Utils/UI/GUI.java
+++ DrakeUtils/src/Utils/UI/GUI.java
@@ -12,19 +12,60 @@ import java.util.Scanner;
 
 public class GUI {
     char [][] guiArray;
+
     int[][] selectionPoints;
     int selectedOption = 0;
+
     char selector = '@';
+    char defaultChar = '0';
+    int[] selectorPos = {1,1};
+    int[] lastPos = {1, 1};
+
+    boolean freeSelect = false;
+    boolean isActive;
     Scanner in = new Scanner(System.in);
 
-    public GUI(char[][] g, int[][] points){
+    //todo: setup a value to set selector
+    public GUI(char[][] g, int[][] points, boolean active, char defChar){
         guiArray = g;
         selectionPoints = points;
+        defaultChar = defChar;
+        isActive = active;
+    }
+    public GUI(char[][]g, boolean isActive, char defChar){
+        guiArray = g;
+        this.isActive = isActive;
+        defaultChar = defChar;
+        freeSelect = true;
     }
 
     public int getSelector(){return selectedOption;}
 
     public void setSelector(int i){selectedOption = i;}
+
+    public boolean IsActive(){ return isActive;}
+    public void setActive(boolean val){  isActive = val;}
+
+    public void moveSelector(int x, int y){
+        //todo: fix so that when characters are at x one they can move left
+        if((x < guiArray[0].length && y < guiArray.length) || (x < 0 && y < 0)) {
+            lastPos[0] = selectorPos[0];
+            lastPos[1] = selectorPos[1];
+
+            selectorPos[0] = x;
+            selectorPos[1] = y;
+        }
+    }
+
+    public int[] getSelectorPos(){
+        int[] pos = {selectorPos[0], selectorPos[1]};
+        return pos;
+    }
+
+    public void RunGUI(){
+        UpdateGUI();
+        DisplayGUI();
+    }
 
     public void getInput(String className, String callMethod, Class<?>[] argTypes, Object[] methArgs){
         String input = in.nextLine().toUpperCase();
@@ -38,9 +79,9 @@ public class GUI {
     }
 
     public void DisplayGUI(){
-        for (int x=0; x < guiArray.length; x++){
+        for (int x = 0; x < guiArray.length; x++) {
             System.out.print("\n");
-            for (int y=0; y < guiArray[0].length; y++){
+            for (int y = 0; y < guiArray[0].length; y++) {
                 System.out.print(ConsoleColors.BLUE + guiArray[x][y] + ConsoleColors.RESET);
             }
         }
@@ -48,12 +89,17 @@ public class GUI {
     }
 
     public void UpdateGUI(){
-        for(int i=0; i < selectionPoints.length; i++){
-            if( i == selectedOption){
-                guiArray[selectionPoints[i][0]][selectionPoints[i][1]] = selector;
-            }else if(i != selectedOption){
-                guiArray[selectionPoints[i][0]][selectionPoints[i][1]] = guiArray[0][0];
+        if(!freeSelect) {
+            for (int i = 0; i < selectionPoints.length; i++) {
+                if (i == selectedOption) {
+                    guiArray[selectionPoints[i][0]][selectionPoints[i][1]] = selector;
+                } else if (i != selectedOption) {
+                    guiArray[selectionPoints[i][0]][selectionPoints[i][1]] = defaultChar;
+                }
             }
+        }else{
+            replaceAt(selectorPos[0], selectorPos[1], selector);
+            replaceAt(lastPos[0], lastPos[1], defaultChar);
         }
     }
 
@@ -62,6 +108,12 @@ public class GUI {
             return guiArray[x][y];
         }else{
             return ' ';
+        }
+    }
+
+    public void replaceAt(int x, int y, char newChar){
+        if(x < guiArray.length && y < guiArray[0].length) {
+            guiArray[x][y] = newChar;
         }
     }
 }
