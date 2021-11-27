0e3af37e7ab30c9f2e801da33b7b1e9f1f51421b
Drakith <drakejwork@gmail.com>
2021-11-27 15:07:28 -0500

    Cleaned Up gui class

--- DrakeUtils/src/Utils/UI/GUI.java
+++ DrakeUtils/src/Utils/UI/GUI.java
@@ -13,61 +13,25 @@ import java.util.Scanner;
 
 public class GUI {
     Tile [][] guiArray;
-
-    Vector2[] selectionPoints;
-    int selectedOption = 0;
-
-    Tile selector = new Tile('@', new Vector2(1,1));
-    Tile defaultChar = new Tile(' ');
-    Vector2 lastPos = new Vector2(1,1);
-
-    boolean freeSelect = false;
-    boolean isActive;
+    boolean isActive = true;
     Scanner in = new Scanner(System.in);
 
     //todo: setup a value to set selector
-    public GUI(Tile[][] g, Vector2[] points, boolean active, Tile defChar){
+    public GUI(Tile[][] g, boolean active){
         guiArray = g;
-        selectionPoints = points;
-        defaultChar = defChar;
         isActive = active;
     }
     public GUI(Tile[][]g, boolean isActive, Tile defChar){
         guiArray = g;
         this.isActive = isActive;
-        defaultChar = defChar;
-        freeSelect = true;
     }
-    public GUI(char[][] g, Vector2[] points, boolean active, char defChar){
+    public GUI(char[][] g, boolean active){
         guiArray = Utility.charToTile(g);
-        selectionPoints = points;
-        defaultChar = Utility.charToTile(defChar);
         isActive = active;
     }
-    public GUI(char[][]g, boolean isActive, char defChar){
-        guiArray = Utility.charToTile(g);
-        this.isActive = isActive;
-        defaultChar = Utility.charToTile(defChar);
-        freeSelect = true;
-    }
-
-
-    public int getSelector(){return selectedOption;}
-
-    public void setSelector(int i){selectedOption = i;}
 
     public boolean IsActive(){ return isActive;}
     public void setActive(boolean val){  isActive = val;}
-
-    public void moveSelector(float x, float y){
-        //todo: fix so that when characters are at x one they can move left
-        if((x < guiArray[0].length && y < guiArray.length) || (x < 0 && y < 0)) {
-            lastPos.x = selector.getGridPos().x;
-            lastPos.y = selector.getGridPos().y;
-
-            selector.setGridPos(new Vector2(x, y));
-        }
-    }
 
     public void RunGUI(){
         UpdateGUI();
@@ -96,18 +60,7 @@ public class GUI {
     }
 
     public void UpdateGUI(){
-        if(!freeSelect) {
-            for (int i = 0; i < selectionPoints.length; i++) {
-                if (i == selectedOption) {
-                    guiArray[(int)selectionPoints[i].x][(int)selectionPoints[i].y] = selector;
-                } else if (i != selectedOption) {
-                    guiArray[(int)selectionPoints[i].x][(int)selectionPoints[i].y] = defaultChar;
-                }
-            }
-        }else{
-            replaceAt(selector.getGridPos().x, selector.getGridPos().y, selector);
-            replaceAt(lastPos.x, lastPos.y, defaultChar);
-        }
+
     }
 
     public Tile findTileAt(int x, int y){
